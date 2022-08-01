43575793d8702c97d0a528c369c2fa321da6d957
Sonnieson <mutiriaesther9@gmail.com>
2022-08-01 10:27:44 +0300

    first commit

---
+++ app/src/main/java/com/example/tab/CodeFragment.kt
@@ -0,0 +1,16 @@
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.tab.R
+
+class CodeFragment : Fragment() {
+    // inflate the layout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        inflater.inflate(R.layout.fragment_code, container, false)!!
+}
---
+++ app/src/main/java/com/example/tab/GeeksFragment.kt
@@ -0,0 +1,15 @@
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.example.tab.R
+
+class GeeksFragment : Fragment() {
+    // inflate the layout
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        inflater.inflate(R.layout.fragment_geeks, container, false)!!
+}
